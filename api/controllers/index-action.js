3e92ccb43dd28093e8ca622dd2b6af5f239e24f0
Urielge <urielge@comunidad.unam.mx>
2019-07-25 01:37:49 -0500

    Feat: carrousel and home design

---
+++ api/controllers/index-action.js
@@ -0,0 +1,11 @@
+/**
+ * IndexController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+ module.exports = function(req,res){
+  // console.log('in action');
+	return res.view('home',{});
+}
